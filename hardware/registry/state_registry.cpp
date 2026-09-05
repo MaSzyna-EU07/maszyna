be48b6f013fe68c1d55743a10af67416819f4634
Hirek193 <polomarket73@gmail.com>
2026-09-06 00:59:46 +0200

    Add engine voltage parameter

--- hardware/registry/state_registry.cpp
+++ hardware/registry/state_registry.cpp
@@ -125,6 +125,7 @@ state_registry::state_registry()
 	add( "hv_current_1", value_type::float32, "A", -5000.0, 5000.0, []( state_snapshot const &s ) { return make_value( s, value_type::float32, s.train.hv_current[ 0 ], s.electric ); } );
 	add( "hv_current_2", value_type::float32, "A", -5000.0, 5000.0, []( state_snapshot const &s ) { return make_value( s, value_type::float32, s.train.hv_current[ 1 ], s.electric ); } );
 	add( "hv_current_3", value_type::float32, "A", -5000.0, 5000.0, []( state_snapshot const &s ) { return make_value( s, value_type::float32, s.train.hv_current[ 2 ], s.electric ); } );
+	add( "engine_voltage", value_type::float64, "V", 0, 40000.0, []( state_snapshot const &s ) { return make_value( s, value_type::float64, s.train.enginevoltage, s.electric ); } );
 	add( "lv_voltage", value_type::float32, "V", 0.0, 200.0, []( state_snapshot const &s ) { return make_value( s, value_type::float32, s.train.lv_voltage ); } );
 	add( "distance", value_type::float64, "m", {}, {}, []( state_snapshot const &s ) { return make_value( s, value_type::float64, s.train.distance ); } );
 
